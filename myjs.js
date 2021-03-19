573433962841705120e5108d15baa903e0fd4f25
Mariana Montes <mariana.montes@kuleuven.be>
2021-03-19 10:32:50 +0100

    too many advances to count, sorry

---
+++ myjs.js
@@ -0,0 +1,10 @@
+
+remark.macros.color = function (hex) {
+  const OkabeIto = [
+    "#E69F00", "#56B4E9", "#009E73", "#F0E442",
+    "#0072B2", "#D55E00", "#CC79A7", "#999999"]
+  const text = this;
+  if (parseInt(hex) >= 0) hex = OkabeIto[parseInt(hex)];
+  return '<span style="color:' + hex + '">' + text + "</span>";
+  };
+  
